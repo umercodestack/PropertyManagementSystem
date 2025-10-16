a8e248b8c9b6d9f9c0e612196ec6a2f56c85d9e7
Muhammad Umer <omer28dec@gmail.com>
2025-10-16 19:38:32 +0400

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+    server: {
+        hmr: {
+            host: "https://admin.livedin.co/public",
+        },
+    },
+    plugins: [
+        vue(),
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            vue: "vue/dist/vue.esm-bundler.js",
+            "@": "/resources/js",
+        },
+    },
+});
